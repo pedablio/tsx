0276e1aefd83ecf151e43c74b5402c1b296af1f7
Hiroki Osame <osame@squareup.com>
2023-01-28 20:53:24 +0900

    Published branch "ts-file-matcher"

---
+++ dist/pkgroll_create-require-be6910af.cjs
@@ -0,0 +1,11 @@
+'use strict';
+
+var module$1 = require('module');
+
+var require$1 = (
+			false
+				? /* @__PURE__ */ module$1.createRequire((typeof document === 'undefined' ? new (require('u' + 'rl').URL)('file:' + __filename).href : (document.currentScript && document.currentScript.src || new URL('pkgroll_create-require-be6910af.cjs', document.baseURI).href)))
+				: require
+		);
+
+exports.require = require$1;
